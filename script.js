58e62bea2d711c4c8b09bd96bccfb7d193637065
Jeremy <43036421+jplegacy@users.noreply.github.com>
2019-04-01 23:40:10 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $(".main").hide();
+$(".startButton2").hide();
+$(".sBox").hide();
 
+var secret = 0;
 
 $(".startButton").click(function() {
     $(".startingScreen").fadeOut(300);
     $(".main").delay(300).fadeIn(300);
    
+});
+
+$(".startButton1").click(function() {
+
+    $(".main").fadeOut(300);
+     $(".startingScreen").delay(300).fadeIn(300);  
+    $(".startButton2").show();
+});
+$(".startButton2").click(function() {
+    
+    $(".startingScreen").fadeOut(300);  
+    $(".sBox").delay(300).fadeIn(300);
+    $("body").css("background","url('https://media.giphy.com/media/26u4lOMA8JKSnL9Uk/giphy.gif')");
+     $("body").css("animation","none");
 });
